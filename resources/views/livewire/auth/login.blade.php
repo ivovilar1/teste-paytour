f6cef33afb08afc3c7d6aa3a26642b361cedfcf7
Ivo Vilar <ivo@grupovoz.dev.br>
2024-04-30 11:13:54 -0300

    frontend: add login page

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -1,3 +1,22 @@
-<div>
-    {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}
-</div>
+<x-card title="Login" shadow class="mx-auto w-[450px] bg-gray-100 dark:bg-gray-900">
+
+    @if($errors->hasAny(['invalidCredentials']))
+        <x-alert icon="o-exclamation-triangle" class="alert-warning mb-4">
+            @error('invalidCredentials')
+            <span>{{ $message }}</span>
+            @enderror
+        </x-alert>
+    @endif
+
+    <x-form wire:submit="login">
+        <x-input label="Email" wire:model="email" />
+        <x-input label="Password" wire:model="password" type="password"/>
+        <x-slot:actions>
+            <div class="w-full flex items-center justify-center">
+                <div>
+                    <x-button label="Login" class="bg-purple-200" type="submit" spinner="submit" icon="o-paper-airplane"/>
+                </div>
+            </div>
+        </x-slot:actions>
+    </x-form>
+</x-card>
