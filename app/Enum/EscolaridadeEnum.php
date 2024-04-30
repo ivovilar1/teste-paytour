487392e2a26b6d23156aed3379017858800108b0
Ivo Vilar <ivo@grupovoz.dev.br>
2024-04-30 17:18:55 -0300

    backend: add getNames for escolaridade enum

--- app/Enum/EscolaridadeEnum.php
+++ app/Enum/EscolaridadeEnum.php
@@ -5,5 +5,18 @@ namespace App\Enum;
 enum EscolaridadeEnum: int
 {
     case MEDIO = 0;
-    case SUPERIOR = 1;
+    case MEDIO_INCOMPLETO = 1;
+    case SUPERIOR_COMPLETO = 2;
+    case SUPERIOR_INCOMPLETO = 3;
+
+    public function getName(): string
+    {
+        return match ($this) {
+            self::MEDIO => 'Ensino Médio Completo',
+            self::MEDIO_INCOMPLETO => 'Ensino Médio Incompleto',
+            self::SUPERIOR_COMPLETO => 'Ensino Superior Completo',
+            self::SUPERIOR_INCOMPLETO => 'Ensino Superior Incompleto',
+            default => 'Não Informado'
+        };
+    }
 }
