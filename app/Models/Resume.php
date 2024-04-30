f0a26fe799d4504d1fee15bf40718a6727f50714
Ivo Vilar <ivo@grupovoz.dev.br>
2024-04-30 11:40:59 -0300

    backend: add migration and model for entity resume

---
+++ app/Models/Resume.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Resume extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'nome',
+        'email',
+        'telefone',
+        'cargo',
+        'escolaridade',
+        'observacoes',
+        'arquivo',
+        'data_envio',
+    ];
+}
